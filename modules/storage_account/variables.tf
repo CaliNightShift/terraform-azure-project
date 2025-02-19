8d9abc55fc279a7235bbaa76165cecdeb2b72a52
Neil Newman <calinightshift@gmail.com>
2025-02-19 09:16:09 -0800

    Update variables.tf

--- modules/storage_account/variables.tf
+++ modules/storage_account/variables.tf
@@ -1,1 +1,15 @@
-
+variable "storage_account_name" {
+description = "Name of the Azure Storage Account"
+type = string
+default = "CNS_MODSA_VAR_SA"
+P a g e | 49
+}
+variable "resource_group_name" {
+description = "Name of the Resource Group"
+type = string
+default = "CNS_MODSA_VAR_RG"
+}
+variable "location" {
+description = "Azure location"
+type = string
+}
