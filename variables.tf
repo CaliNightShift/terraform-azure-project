529264400f00b04ca9141087b7c613de1ff929bc
Neil Newman <calinightshift@gmail.com>
2025-02-19 09:23:00 -0800

    Update variables.tf
    Fixed errors

--- variables.tf
+++ variables.tf
@@ -1,7 +1,6 @@
 variable "resource_group_name" {
 description = "Name of the Azure Resource Group"
 type = string
-default = "CNS_Git_RG"
 }
 variable "location" {
 description = "Azure region for resources"
@@ -11,5 +10,5 @@ default = "East US"
 variable "storage_account_name" {
 description = "Name of the Azure Storage Account"
 type = string
-default = "diveronegreat12"
+default = "CNS_GIT_PROJ_SA"
 }
