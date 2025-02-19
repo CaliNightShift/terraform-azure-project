98cd164dbe4beb3211c8b3fb00cd1b31c32c0a4f
Neil Newman <calinightshift@gmail.com>
2025-02-19 09:14:11 -0800

    Update variables.tf

--- modules/resource_group/variables.tf
+++ modules/resource_group/variables.tf
@@ -1,1 +1,9 @@
-
+variable "resource_group_name" {
+description = "Name of the Azure Resource Group"
+type = string
+default = "CNS_Git_MODRG_RG"
+}
+variable "location" {
+description = "Azure location"
+type = string
+}
